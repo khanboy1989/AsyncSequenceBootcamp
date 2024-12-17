44646cac14942b7135ac969bf669348ca309dde7
Serhan Khan <serhankhan0@gmail.com>
2024-12-17 10:44:38 +0200

    Initial Commit

---
+++ AsyncSequenceAndStreamBootcamp/AsyncSequenceAndStreamBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSequenceAndStreamBootcampApp.swift
+//  AsyncSequenceAndStreamBootcamp
+//
+//  Created by Serhan Khan on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSequenceAndStreamBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
